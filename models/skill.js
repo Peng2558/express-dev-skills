79cbc9e22ef6e3b0aff81ee4e7b725e73a92d81a
mikega1354 <tabrizimike@gmail.com>
2023-11-24 18:30:07 -0800

    Adds Models

---
+++ models/skill.js
@@ -0,0 +1,21 @@
+const skills = [
+    {id: 1, skill: 'JavaScript', Indorsed: true},
+    {id: 2, skill: 'HTML', Indorsed: false},
+    {id: 3, skill: 'CSS', Indorsed: true},
+    {id: 4, skill: 'Express', Indorsed: false}
+  ];
+  
+  module.exports = {
+    getAll,
+    getOne
+  };
+  
+  
+  function getAll() {
+    return skills;
+  }
+  
+  function getOne(id) {
+    id = parseInt(id)
+    return skills.find(skill => skill.id === id)
+  } 
